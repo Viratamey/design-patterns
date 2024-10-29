4402dee0fec7def0ced140c0db970800b5c6fd27
Amey Pawar <amey.pawar137@gmail.com>
2024-10-29 12:02:52 +0530

    added creational design patterns

---
+++ src/abstract_factory/pattern/Engineer.java
@@ -0,0 +1,10 @@
+package abstract_factory.pattern;
+
+public class Engineer implements Profession{
+
+	@Override
+	public void print() {
+		System.out.println("In Print of Engineer class");
+	}
+
+}
